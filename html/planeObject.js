c7b57bd9f95a2d44e00710f6119366493ee944bb
fixfox <fixfox2@gmail.com>
2024-08-22 22:43:45 +0000

    Resolving merge conflicts

--- html/planeObject.js
+++ html/planeObject.js
@@ -882,7 +882,7 @@ PlaneObject.prototype.updateIcon = function() {
             }
             if (this.wd != null) {
                 if (showLabelUnits) {
-                    labelText += format_track_arrow((this.wd + 180 % 360)) + NBSP + this.wd + '°' + NBSP;
+                    labelText += format_track_arrow((this.wd + 180 % 360)) + NBSP + this.wd + '??' + NBSP;
                     labelText += format_speed_long(this.ws, DisplayUnits);
                 } else {
                     labelText += format_track_arrow((this.wd + 180 % 360)) + NBSP + this.wd + NBSP;
@@ -2710,7 +2710,7 @@ PlaneObject.prototype.checkVisible = function() {
     const refresh = lastRefreshInt / 1000;
     const noInfoTimeout = replay ? 600 : (reApi ? (30 + 2 * refresh) : (30 + Math.min(1, (globeTilesViewCount / globeSimLoad)) * (2 * refresh)));
     const modeSTime = (guessModeS && this.dataSource == "modeS") ? 300 : 0;
-    const tisbReduction = (adsbexchange && this.icao[0] == '~') ? 15 : 0;
+    const tisbReduction = (globeIndex && this.icao[0] == '~') ? 15 : 0;
     // If no packet in over 58 seconds, clear the plane.
     // Only clear the plane if it's not selected individually
 
